ef1482bcc3faa449fe6237566ea563cc38d00d7b
yuan.xiong <yuan.xiong@intel.com>
2023-06-02 13:42:54 +0800

    Add Text-To-Video with Pose Control demo.
    Signed-off-by: yuan.xiong <yuan.xiong@intel.com>

--- demo/demo-t2v.py
+++ demo/demo-t2v.py
@@ -3,8 +3,6 @@ import sys
 import torch
 from pathlib import Path
 sys.path.append(str(Path(__file__).absolute().parent.parent))
-
-
 os.environ['CURL_CA_BUNDLE'] = ''
 
 from model import Model
@@ -16,9 +14,13 @@ print(f'--> model {model}')
 prompt = "A horse galloping on a street"
 params = {"t0": 44, "t1": 47 , "motion_field_strength_x" : 12, "motion_field_strength_y" : 12, "video_length": 8}
 # more options
-params.update({"chunk_size" : 2})
+params.update({"chunk_size" : 1})
 params.update({"merging_ratio" : 1})
 print(f'params: {params}')
 
-out_path, fps = f"./text2video_{prompt.replace(' ','_')}.mp4", 4
-model.process_text2video(prompt, fps = fps, path = out_path, **params)
+#out_path, fps = f"./text2video_{prompt.replace(' ','_')}.mp4", 4
+#model.process_text2video(prompt, fps = fps, path = out_path, **params)
+
+motion_path = '__assets__/poses_skeleton_gifs/dance1_corr.mp4'
+out_path = f"./text2video_pose_guidance_{prompt.replace(' ','_')}.gif"
+model.process_controlnet_pose(motion_path, prompt=prompt, save_path=out_path)
