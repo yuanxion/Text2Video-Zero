bc9cafe11ccc1dfb15b4e4df01851ea2d92c439d
yuan.xiong <yuan.xiong@intel.com>
2023-08-11 07:49:26 +0800

    Add edge-tts for voice (*.mp3, *.tts)
    Signed-off-by: yuan.xiong <yuan.xiong@intel.com>

--- demo/demo-t2v-tts.py
+++ demo/demo-t2v-tts.py
@@ -1,11 +1,12 @@
 import os
 import shlex
+import subprocess
 import sys
 import torch
 from pathlib import Path
 
 
-def read_and_splite(story_file):
+def read_and_splite(story_file, txt_folder: Path):
     with open(story_file, 'r') as f:
         lines = f.readlines()
 
@@ -16,17 +17,15 @@ def read_and_splite(story_file):
         # skip blank line
         if line == '':
             continue
-
         #if not i == 12:
         #    continue
 
-        print(f'[line {i}] {line}')
+        # print(f'[line {i}] {line}')
         lex = shlex.shlex(line, posix=True)
         lex.whitespace = '.'
         lex.whitespace_split = True
         lex.quotes = '"'
         res = list(lex)
-
         # # sq = shlex.quote(line)
         # # res = shlex.split(sq, comments='.', posix=True)
         # print(f'res {i}: {res=}')
@@ -38,14 +37,12 @@ def read_and_splite(story_file):
 
             # remove white-space
             sentence = sentence.strip()
-
             sentences.append(sentence)
 
-        # if i == 4:
-        #     sys.exit(0)
+    # for j, sentence in enumerate(sentences):
+    #     print(f'  [sentence {j}] {sentence}')
 
-    for j, sentence in enumerate(sentences):
-        print(f'  [sentence {j}] {sentence}')
+    return sentences
 
 def demo_t2v(prompt: str):
     sys.path.append(str(Path(__file__).absolute().parent.parent))
@@ -71,6 +68,38 @@ def demo_t2v(prompt: str):
     model.process_text2video(prompt, fps=fps, path=out_path, **params)
 
 
+def demo_tts(prompt: str, mp3_folder: Path):
+    mp3_file = f'{mp3_folder}/{prompt}.mp3'
+    tts_file = f'{mp3_folder}/{prompt}.tts'
+
+    curr_dir = os.getcwd()
+    voice_dir = os.path.join(curr_dir, mp3_folder)
+    os.makedirs(voice_dir, exist_ok=True)
+
+    command = ['edge-tts']
+    command += ['--voice', 'zh-CN-XiaoyiNeural']
+    command += ['--text', prompt]
+    command += ['--write-media', mp3_file]
+    command += ['--write-subtitles', tts_file]
+
+    try:
+        result = subprocess.run(command, timeout=10)
+        print(result)
+    except subprocess.CalledProcessError as e:
+        print('Command: {command}')
+        print('Exception return code: {e.returncode}')
+        print('Exception output: {e.output}')
+
+def demo_t2v_tts(sentence, mp3_folder: Path, mp4_folder: Path):
+    print(f'demo_t2v_tts {sentence=}')
+    demo_tts(sentence, mp3_folder)
+
+txt_folder = './texts'
+mp3_folder = './voices'
+mp4_folder = './videos'
+
 story = 'demo/tadpoles_look_for_mom.txt'
-sentences = read_and_splite(story)
-#demo_t2v(sentence)
+sentences = read_and_splite(story, txt_folder)
+
+for sentence in sentences:
+    demo_t2v_tts(sentence, mp3_folder, mp4_folder)
